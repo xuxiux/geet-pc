befa6f9c7369808f80b38498b2f743f7659a83e7
xxx <xxx@qq.com>
2022-11-14 22:17:58 +0800

    修改全局样式

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import  ReactDOM  from 'react-dom'
+import  ReactDOM  from 'react-dom/client'
 import './index.scss'
 import App from './App'
 
 
-ReactDOM.render(
+ReactDOM.createRoot(document.querySelector('#root')).render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>
-    , document.querySelector('#root')) 
+    ) 
